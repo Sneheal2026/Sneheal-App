888e777ab7a6cace6e2edcdd20bf6ef69b8190fc
Sneheal2026 <dev@sneheal.com>
2026-08-21 16:45:10 +0530

    Firabase updated keys added !

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getDatabase, ref, set, remove, onValue, type Unsubscribe } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdklh229UjBHoObtARDn-KI3LObnqr6Eo',
-  authDomain: 'snehealll.firebaseapp.com',
-  databaseURL: 'https://snehealll-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'snehealll',
-  storageBucket: 'snehealll.firebasestorage.app',
-  messagingSenderId: '842812341947',
-  appId: '1:842812341947:web:e42582c838ee744514739e',
+  apiKey: 'AIzaSyA7aId8EntOYArO3vfRzjkydyJf1jGQJRM',
+  authDomain: 'sneheal.firebaseapp.com',
+  databaseURL: 'https://sneheal-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'sneheal',
+  storageBucket: 'sneheal.firebasestorage.app',
+  messagingSenderId: '806572974016',
+  appId: '1:806572974016:web:748d49acbd4f683bfb3225',
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
